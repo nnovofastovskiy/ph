9bbd884c1f08db2dd4ded39da66b2e1b3d2fdf3d
Nikita <bananchik26@gmail.com>
2022-07-08 15:36:49 +0300

    auth

---
+++ ph-nextjs/page_components/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import styles from "./HomePage.module.css";
+import { HomePageProps } from "./HomePage.props";
+
+const HomePage = ({ }: HomePageProps): JSX.Element => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default HomePage;
